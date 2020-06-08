c69c0a693330bfdab55ef91da1e55b4291d4fd6f
TrueSide1005 <ralu_florina@yahoo.com>
2020-06-08 19:57:37 +0300

    Update map-routes.php

--- twApp/app/api/map-routes.php
+++ twApp/app/api/map-routes.php
@@ -43,8 +43,18 @@ $mapRoutes = [
     ],
     [
         "method" => "GET",
-        "route" => "map/:month/:id",
+        "route" => "map/judet/:judet",
+        "handler" => "getMapRoutesCompleteJudet"
+    ],
+    [
+        "method" => "GET",
+        "route" => "map/:month/id/:id",
         "handler" => "getMapRoutesId"
+    ],
+    [
+        "method" => "GET",
+        "route" => "map/:month/judet/:judet",
+        "handler" => "getMapRoutesJudet"
     ],
 ];
 //conexiunea cu baza de date
@@ -361,8 +371,29 @@ function getMapRoutesComplete($req)
     Response::status(200);
     require __DIR__ . '/../config.php';
     $param = "{$req['params']['id']}";
-    if ($param <= 41 /*&& is_numeric($param)*/) {
+    if ($param <= 41 && is_numeric($param)) {
         $stmt = $conn->prepare("SELECT * FROM data WHERE  id = ?");
+        $stmt->bind_param('s', $param);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $data = array();
+        foreach ($result as $row) {
+            $data[] = $row;
+        }
+        Response::json($data);
+    } else {
+        handle404();
+    }
+}
+
+function getMapRoutesCompleteJudet($req)
+{
+    //echo "getMapRoutesComplete";
+    Response::status(200);
+    require __DIR__ . '/../config.php';
+    $param = "{$req['params']['judet']}";
+    if (is_string($param)) {
+        $stmt = $conn->prepare("SELECT * FROM data WHERE  nume = ?");
         $stmt->bind_param('s', $param);
         $stmt->execute();
         $result = $stmt->get_result();
@@ -554,6 +585,69 @@ function getMapRoutesBarbatiId($req)
     $param = "{$req['params']['id']}";
     if ($coloana != 404 && $param <= 41 && is_numeric($param)) {
         $stmt = $conn->prepare("SELECT id, nume, " . $coloana . " FROM data WHERE  id = ?");
+        $stmt->bind_param('s', $param);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $data = array();
+        foreach ($result as $row) {
+            $data[] = $row;
+        }
+
+        Response::json($data);
+    } else {
+        handle404();
+    }
+}
+
+function getMapRoutesJudet($req)
+{
+    Response::status(200);
+    $coloana = "";
+    switch ($req['params']['month']) {
+        case 'martie2020':
+            $coloana = "martie2020_total, martie2020_femei, martie2020_barbati";
+            break;
+        case 'februarie2020':
+            $coloana = "februarie2020_total, februarie2020_femei, februarie2020_barbati";
+            break;
+        case 'ianuarie2020':
+            $coloana = "ianuarie2020_total, ianuarie2020_femei, ianuarie2020_barbati";
+            break;
+        case 'decembrie2019':
+            $coloana = "decembrie2019_total, decembrie2019_femei, decembrie2019_barbati";
+            break;
+        case 'noiembrie2019':
+            $coloana = "noiembrie2019_total, noiembrie2019_femei, noiembrie2019_barbati";
+            break;
+        case 'octombrie2019':
+            $coloana = "octombrie2019_total, octombrie2019_femei, octombrie2019_barbati";
+            break;
+        case 'septembrie2019':
+            $coloana = "septembrie2019_total, septembrie2019_femei, septembrie2019_barbati";
+            break;
+        case 'august2019':
+            $coloana = "august2019_total, august2019_femei, august2019_barbati";
+            break;
+        case 'iulie2019':
+            $coloana = "iulie2019_total, iulie2019_femei, iulie2019_barbati";
+            break;
+        case 'iunie2019':
+            $coloana = "iunie2019_total, iunie2019_femei, iunie2019_barbati";
+            break;
+        case 'mai2019':
+            $coloana = "mai2019_total, mai2019_femei, mai2019_barbati";
+            break;
+        case 'aprilie2019':
+            $coloana = "aprilie2019_total, aprilie2019_femei, aprilie2019_barbati";
+            break;
+        default:
+            $coloana = "404";
+            break;
+    }
+    require __DIR__ . '/../config.php';
+    $param = "{$req['params']['judet']}";
+    if ($coloana != "404" && is_string($param) && !is_numeric($param)) {
+        $stmt = $conn->prepare("SELECT id, nume, " . $coloana . " FROM data WHERE nume = ?");
         $stmt->bind_param('s', $param);
         $stmt->execute();
         $result = $stmt->get_result();
