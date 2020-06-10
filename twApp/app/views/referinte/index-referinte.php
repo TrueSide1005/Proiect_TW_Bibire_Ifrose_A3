c67135f0bfbfe6b75b29677ce6030f64f07f5134
AnaIfrose <45734425+AnaIfrose@users.noreply.github.com>
2020-06-10 07:50:24 +0300

    git fixinig

--- twApp/app/views/referinte/index-referinte.php
+++ twApp/app/views/referinte/index-referinte.php
@@ -7,7 +7,7 @@
     <link async rel="shortcut icon" type="image/x-icon" href="/public/images/flag.ico" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" type="text/css" href="/public/css/menu.css" />
-    <link rel="stylesheet" type="text/css" href="/public/css/referinte.css" />
+    <link rel="stylesheet" type="text/css" href="/public/css/referinte.css" /> 
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
 
